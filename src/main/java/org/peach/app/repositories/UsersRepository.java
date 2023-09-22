1139d6c8017c9485fb4b2f9e6711af34c55c89e3
онстантин ильчитский <kilatos@yandex.ru>
2023-09-22 14:55:58 +0300

    replacing jdbc api to jdbc template

--- src/main/java/org/peach/app/repositories/UsersRepository.java
+++ src/main/java/org/peach/app/repositories/UsersRepository.java
@@ -1,6 +1,9 @@
 package org.peach.app.repositories;
 
 import org.peach.app.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
@@ -9,82 +12,34 @@ import java.util.List;
 
 @Component
 public class UsersRepository {
-    private static final String URL = "jdbc:postgresql://localhost:5432/peach";
-    private static final String username = "postgres";
-    private static final String password = "qwerty007";
 
-
-    private static Connection connection;
-    static {
-        try{
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection = DriverManager.getConnection(URL,username,password);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    private final JdbcTemplate jdbcTemplate;
+    public UsersRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
+
+
+
+
+
     public User findOne(long id){
-        try(Statement statement = connection.createStatement()){
-            String SQL = String.format("SELECT * FROM users where id = %d", id);
-            ResultSet resultSet = statement.executeQuery(SQL);
-            User curUser = new User();
-            while (resultSet.next()){
-                curUser.setId(resultSet.getInt("id"));
-                curUser.setName(resultSet.getString("name"));
-            }
-            return curUser;
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-        return new User();
+        return jdbcTemplate.query("SELECT * FROM users where id = ?",  new UsersMapper(), id).stream().
+                findAny().orElse(null);
     }
     public void updateOne(long id, User updatedUser){
-        try(PreparedStatement preparedStatement = connection.prepareStatement("update users set id = ? , name = ? where id = ?")){
-            preparedStatement.setLong(1,updatedUser.getId());
-            preparedStatement.setString(2,updatedUser.getName());
-            preparedStatement.setLong(3,id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("update users set id = ?, name = ? where id = ?",updatedUser.getId(),updatedUser.getName(), id );
+
 
     }
-
     public List<User> index(){
-        List<User> array = new ArrayList<>();
-        try (Statement statement = connection.createStatement()){
-            String SQL = "SELECT * FROM users";
-            ResultSet resultSet = statement.executeQuery(SQL);
-            while (resultSet.next()){
-                User curUser = new User();
-                curUser.setId(resultSet.getInt("id"));
-                curUser.setName(resultSet.getString("name"));
-                array.add(curUser);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return array;
+        return jdbcTemplate.query("SELECT * FROM users", new BeanPropertyRowMapper<>(User.class));
     }
     public void save(User user){
-        try(PreparedStatement preparedStatement = connection.prepareStatement("insert into users values (?,?)")){
-            preparedStatement.setLong(1,user.getId());
-            preparedStatement.setString(2,user.getName());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("insert into users values (?,?)", user.getId(),user.getName());
+
     }
     public void delete(long id){
-        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users where id = ?")){
-            preparedStatement.setLong(1,id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("delete from users where id = ?", id);
+
     }
 }
