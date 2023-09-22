f8c2ae900cc04d4a58de70827bc2d4ebe7afcf56
онстантин ильчитский <kilatos@yandex.ru>
2023-09-22 14:56:17 +0300

    replacing jdbc api to jdbc template

---
+++ src/main/java/org/peach/app/repositories/UsersMapper.java
@@ -0,0 +1,19 @@
+package org.peach.app.repositories;
+
+import org.peach.app.models.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UsersMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        User curUser = new User();
+
+        curUser.setId(resultSet.getInt("id"));
+        curUser.setName(resultSet.getString("name"));
+        return curUser;
+    }
+}
